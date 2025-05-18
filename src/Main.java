d53118290585630664350eaa8ecb5d1c5c429364
Rustam <kutrustam1991@gmail.com>
2025-05-18 18:26:46 +0500

    commit4

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("Привет, Нетология!!!");
+       System.out.println("2 + 3");
+       System.out.println(2 + 3);
+
+        int myMaxSpeed = 17;
+        int x = myMaxSpeed + 3;
+
+        double d = 6.5;
+
+        String name = "Rustam";
+
+        System.out.println(name + " ехал со скоростью "+ myMaxSpeed);
+    }
+}
