c788cb181d7ed9496377f497ad7cd23019160c32
Rustam <kutrustam1991@gmail.com>
2025-05-18 19:32:01 +0500

    commit5

---
+++ src/Main3.java
@@ -0,0 +1,13 @@
+public class Main3 {
+    public static void main(String[] args) {
+
+        int income = 78600; // доход
+        int spending = 100000; // расход
+
+        int tax = income * 6 / 100; // налог 6% на доходы
+        int tax2 = (income - spending) * 15 / 100; // налог 15% на разницу доходов и расходов
+
+        System.out.println("Ваш налог на первой системе: " + tax + " рублей");
+        System.out.println("Ваш налог на второй системе: " + tax2 + " рублей");
+    }
+}
